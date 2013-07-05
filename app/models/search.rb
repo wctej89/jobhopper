802c6e49d077a44d398ac49dedc1f2278bc0c8bd
Neil Shah <neilshahteaches@gmail.com>
2013-07-05 00:35:43 -0700

    half of the algo

--- app/models/search.rb
+++ app/models/search.rb
@@ -7,4 +7,22 @@ module Search
   def fuzzy_query(query_string)
     query_string.strip.gsub(/~/, "").gsub(/\W+/, ",").split(",").map! { |k| "#{k}~0.7" }.join("+")
   end
+
+  def search_tags(query_string, user_location)
+    tag_searches = Tag.search(query_string)
+    tag_jobs = []
+    tag_searches.each do |tag|
+      tag_jobs << tag.jobs
+    end
+    tag_jobs
+  end
+
+  def search_jobs(query_string, user_location)
+    job_searches = Job.search(query_string)
+    jobs = []
+    job_searches.each do |job|
+      jobs << job
+    end
+    jobs
+  end
 end
