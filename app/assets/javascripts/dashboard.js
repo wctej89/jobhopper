ed801816d6d0e7bfe04af9623c58079bbaa421e7
dbcapprentice <dbcapprentice@dbc.com>
2013-07-06 01:35:35 -0700

    removing of tags on profile working

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,15 +1,24 @@
 $(document).ready(function(){
 
-   $(document).on('click', '.remove_from_queue', function(e){
-      e.preventDefault();
-      var job = this
+  $(document).on('click', '.remove_tag', function(e){
+    e.preventDefault();
+    var $tag = $(this)
+    $.ajax({
+      type: 'delete',
+      url: '/user_tags/' + $tag.attr('id')
+    }).success(function(response){
+      $tag.remove();
+    });
+  }); 
+
+ $(document).on('click', '.remove_from_queue', function(e){
+    e.preventDefault();
+    var job = this
     $.ajax({
       method: "GET",
       url: $(this).attr('href')
     }).success(function(response){
       $(job).closest('li').remove();
     });
-    
   });
-
 });
