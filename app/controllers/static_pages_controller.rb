d217a12c634f5d156eb3b56af585762e15dea6b8
sidazhang <sidazhang89@gmail.com>
2013-07-05 18:54:47 -0700

    I'm not sure what changed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 require 'geokit'
 class StaticPagesController < ApplicationController
   def home
-    p Geokit::Geocoders::GoogleGeocoder3.geocode("#{cookies["lat"]}, #{cookies["lon"]}")
+    # p Geokit::Geocoders::GoogleGeocoder3.geocode("#{cookies["lat"]}, #{cookies["lon"]}")
   end
 
   def location
