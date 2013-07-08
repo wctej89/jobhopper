7e28c7b5ea8d8f8c7237b9607f9a0c7326b3066d
Neil Shah <neilshahteaches@gmail.com>
2013-07-07 20:56:14 -0700

    fuck colorbox

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,8 +4,7 @@ class OmniauthCallbacksController < ApplicationController
     user = User.from_omniauth(auth_hash) 
     if user.persisted?
       sign_in(user)
-      set_cookie(user)
-      redirect_to(user_feed_path(user))
+      wizard(user)
     else
       redirect_to new_user_path
       flash[:notice] = "Please create yourself"
@@ -16,16 +15,13 @@ class OmniauthCallbacksController < ApplicationController
 
   protected
 
-  def set_cookie(user)
-    cookies[:new_user] = true if user.created_at >= (DateTime.now - 10.minutes)
+  def wizard(user)
+    if current_user.created_at >= (DateTime.now - 10.minutes)
+      redirect_to wizard_path
+    else
+      redirect_to user_path(user)
+    end
   end
-  # def wizard(user)
-  #   if current_user.created_at >= (DateTime.now - 10.minutes)
-  #     redirect_to wizard_path
-  #   else
-  #     redirect_to user_path(user)
-  #   end
-  # end
 
   def auth_hash
     request.env['omniauth.auth']
