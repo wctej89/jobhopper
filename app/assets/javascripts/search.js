66def11e0e6f7267d9d6b7b1fcc00b3671204d9c
Neil Shah <neilshahteaches@gmail.com>
2013-07-09 15:18:47 -0700

    got wizard skills working

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,22 +1,41 @@
 $(document).ready(function(){
 
-  $('.wizard-search').on('keyup',_.debounce(function(e){
+  $(document).on('click', 'button.add_to_queue', function(e){
     e.preventDefault();
+    var $target = e.target;
+    e.preventDefault();
+    console.log($($target));
     $.ajax({
-      method: 'get',
-      url: '/search_tags',
-      data: $(this).serialize()
+      method: 'post',
+      url: '/user_tags/',
+      data: {"id":$(this).attr('id')}
     }).success(function(response){
-      console.log(response);
-      for(i=0;i<=response.length;i++)
-      {
-        $('.search_results').append('<li><div class="job-info"><h1>' + response[i].name + '</h1><div class="buttons"></div></li>');
-      }
+      $($target).closest('li').fadeOut();
+      $('.skills-list').append('<button>' + $($target).parent().parent().find('h1').text() +'</button>');
     });
+  });
 
-    if($('.wizard-search').val() === '')
-    {
-      $('.search_results').html('');
+  $('.wizard-search').on('keyup', _.debounce(function(e){
+      e.preventDefault();
+    if(e.keyCode == 8)
+      {
+        $('.search_results').html('');
+      }
+      else
+      {
+        $.ajax({
+        method: 'get',
+        url: '/search_tags',
+        data: $(this).serialize()
+      }).success(function(response){
+        console.log(response);
+        for(i=0;i<=response.length;i++)
+        {
+          $('.search_results').append(
+            '<li><div class="job-info"><h1>' + response[i].name + '</h1><div class="buttons"><button id='+ response[i].id +' class="add_to_queue" href="/user_tags/">Add Skill</button></div></li>'
+            );
+        }
+      });
     }
   }));
 });
--- app/controllers/user_tags_controller.rb
+++ app/controllers/user_tags_controller.rb
@@ -2,9 +2,10 @@ class UserTagsController < ApplicationController
 
 
   def create
-    user = User.find(params["user_tags"]["user_id"].to_i)
-    tags = params["user_tags"]["tags"]
-    tags.each do |tag|
+    cookies[:skills] = "true"
+    user = current_user
+    tag = params[:id]
+    if tag
       user.user_tags.create(:tag_id => tag.to_i)
     end
     if cookies[:skills] == "true"
