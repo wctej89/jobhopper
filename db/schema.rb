0bda8e85a37d8bfcfc24af643b47744f79bd3143
Neil Shah <neilshahteaches@gmail.com>
2013-07-08 20:15:39 -0700

    some css

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130708233906) do
+ActiveRecord::Schema.define(:version => 20130709015341) do
 
   create_table "job_lists", :force => true do |t|
     t.integer  "list_id"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(:version => 20130708233906) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "job_id"
+    t.string   "read"
   end
 
   create_table "tags", :force => true do |t|
