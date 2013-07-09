d415dc21253a4bb4d8051050e0172caf4b6c8d35
Neil Shah <neilshahteaches@gmail.com>
2013-07-09 01:05:02 -0700

    removed some unnecessary files

--- app/controllers/search_controller.rb
+++
@@ -1,7 +0,0 @@
-class SearchController < ApplicationController
-  include Search
-  def index
-    @query = params[:search]
-    @results = query_exact(@query.dup) if @query and !@query.empty?
-  end
-end
